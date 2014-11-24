19fec8a3dff09cda22a43f92a56a9cb26ea55119
Harry Evans <harridunc@gmail.com>
2014-11-24 17:16:02 +0000

    Initial Push New Unity 4.6rc3 project Vuforia Plugin

---
+++ Assets/Scripts/VuforiaHandler/CameraHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHandler : MonoBehaviour, ITrackableEventHandler
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void OnTrackableStateChanged(
+				TrackableBehaviour.Status previousStatus,
+				TrackableBehaviour.Status newStatus)
+		{
+		}
+
+		private void OnTrackingFound()
+		{
+		}
+
+		private void OnTrackingLost()
+		{
+		}
+}

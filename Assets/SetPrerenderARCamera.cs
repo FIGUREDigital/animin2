b70dbeff6d7faf9a2b4b9d7e774c12c4d29e4763
Harry Evans <harridunc@gmail.com>
2014-12-05 13:15:15 +0000

    Attempted fix for Vuforia mismatch on iPhone 6 +. Added missing poo and wee prefabs on characterprogressscript.

---
+++ Assets/SetPrerenderARCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPrerenderARCamera : MonoBehaviour {
+
+    void OnPreRender()
+    {
+        // make sure the correct backface culling is set for this camera
+        GL.SetRevertBackfacing(false);
+    }
+}

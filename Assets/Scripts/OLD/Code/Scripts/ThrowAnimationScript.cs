52b1705a396263a30bda77ed26cb58319200a0ef
Harry Evans <harridunc@gmail.com>
2014-12-11 13:18:27 +0000

    Garden throwing correction

--- Assets/Scripts/OLD/Code/Scripts/ThrowAnimationScript.cs
+++ Assets/Scripts/OLD/Code/Scripts/ThrowAnimationScript.cs
@@ -1,100 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ThrowAnimationScript : MonoBehaviour 
+public class ThrowAnimationScript : MonoBehaviour
 {
-	private enum StateEnum
-	{
-		None = 0,
-		Begin,
-		Throw,
-		End,
-	}
+    private enum StateEnum
+    {
+        None = 0,
+        Begin,
+        Throw,
+        End,
+    }
 
-	StateEnum State;
-	Vector3 Direction;
+    StateEnum State;
+    Vector3 Direction;
 
-	float VerticalSpeed;
-	private float ThrowSpeed = 100;
+    float VerticalSpeed;
+    private float ThrowSpeed = 100;
 
-	public void Begin(Vector3 direction, float speed)
-	{
-		Direction = direction;
-		State = StateEnum.Begin;
-		ThrowSpeed = speed;
+    public void Begin(Vector3 direction, float speed)
+    {
+        Direction = direction;
+        State = StateEnum.Begin;
+        ThrowSpeed = speed;
 
-		//Rigidbody body = this.gameObject.AddComponent<Rigidbody>();
-		//body.AddForce(direction * 100);
+        //Rigidbody body = this.gameObject.AddComponent<Rigidbody>();
+        //body.AddForce(direction * 100);
 
-		//body.velocity = new Vector3(1 * direction.x, -1000, 1 * direction.z);
-		//Physics.gravity = new Vector3(0, -10.0F, 0);
-		VerticalSpeed = -260;
+        //body.velocity = new Vector3(1 * direction.x, -1000, 1 * direction.z);
+        //Physics.gravity = new Vector3(0, -10.0F, 0);
+        VerticalSpeed = -260;
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update () 
-	{
-		VerticalSpeed = VerticalSpeed + (Time.deltaTime * 600);
+    // Update is called once per frame
+    void Update()
+    {
+        VerticalSpeed = VerticalSpeed + (Time.deltaTime * 600);
 
-		switch(State)
-		{
-			case StateEnum.Begin:
-			{
-				this.transform.position += Direction * Time.deltaTime * ThrowSpeed;
-				this.transform.position -= new Vector3(0, VerticalSpeed * Time.deltaTime, 0);
+        switch (State)
+        {
+            case StateEnum.Begin:
+                {
+                    this.transform.position += Direction * Time.deltaTime * ThrowSpeed;
+                    this.transform.position -= new Vector3(0, VerticalSpeed * Time.deltaTime, 0);
 
-				bool isNonArScene = false;
-				if(UIGlobalVariablesScript.Singleton.NonSceneRef.activeInHierarchy)
-				{
-					isNonArScene = true;
-				}
+                    bool isNonArScene = false;
+                    if (UIGlobalVariablesScript.Singleton.NonSceneRef.activeInHierarchy)
+                    {
+                        isNonArScene = true;
+                    }
 				
-				Vector3 expetectedPosition =  new Vector3(this.transform.position.x, UIGlobalVariablesScript.Singleton.MainCharacterRef.transform.position.y, this.transform.position.z);
-				bool hasLanded = false;
+                    Vector3 expetectedPosition = new Vector3(this.transform.position.x, UIGlobalVariablesScript.Singleton.MainCharacterRef.transform.position.y, this.transform.position.z);
+                    bool hasLanded = false;
 
-				if(this.transform.position.y <= UIGlobalVariablesScript.Singleton.MainCharacterRef.transform.position.y)
-				{
-					if(isNonArScene && (this.transform.position.x <= -160 || this.transform.position.x>= 160 || this.transform.position.z <= -160 || this.transform.position.z >= 160))
-					{
-						if(this.transform.position.y <= -350)
-						{
-							//hasLanded = true;
-						this.transform.position =  new Vector3(Random.Range(-140, 140), 100, Random.Range(-140, 140));
-						}
-					}
-					else
-					{
-						hasLanded = true;
-					}
-				}
+                    if (this.transform.position.y <= UIGlobalVariablesScript.Singleton.MainCharacterRef.transform.position.y)
+                    {
+                        int range = 220;
+                        if (isNonArScene && (this.transform.position.x <= -range || this.transform.position.x >= range || this.transform.position.z <= -range || this.transform.position.z >= range))
+                        {
+                            if (this.transform.position.y <= -350)
+                            {
+                                //hasLanded = true;
+                                this.transform.position = new Vector3(Random.Range(-140, 140), 100, Random.Range(-140, 140));
+                            }
+                        }
+                        else
+                        {
+                            hasLanded = true;
+                        }
+                    }
 
-			if(hasLanded)
-			{
-				UIGlobalVariablesScript.Singleton.SoundEngine.Play(GenericSoundId.ItemLand);
+                    if (hasLanded)
+                    {
+                        UIGlobalVariablesScript.Singleton.SoundEngine.Play(GenericSoundId.ItemLand);
 				
-				//Destroy(this.rigidbody);
-				this.transform.position = expetectedPosition;
-				Destroy(this);
+                        //Destroy(this.rigidbody);
+                        this.transform.position = expetectedPosition;
+                        Destroy(this);
 				
-				this.gameObject.layer = LayerMask.NameToLayer("Default");
-			}
+                        this.gameObject.layer = LayerMask.NameToLayer("Default");
+                    }
 
 				
 
-				break;
-			}
+                    break;
+                }
 
-			case StateEnum.Throw:
-			{
-				break;
-			}
+            case StateEnum.Throw:
+                {
+                    break;
+                }
 
-			case StateEnum.End:
-			{
-				break;
-			}
-		}
+            case StateEnum.End:
+                {
+                    break;
+                }
+        }
 	
-	}
+    }
 }
